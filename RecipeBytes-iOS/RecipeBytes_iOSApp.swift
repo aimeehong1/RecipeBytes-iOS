ba74408b1e960e1f92a64daf3dc18579652bc26d
Aimee Hong <honglp@bc.edu>
2024-11-25 17:47:49 -0500

    Initial Commit

---
+++ RecipeBytes-iOS/RecipeBytes_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBytes_iOSApp.swift
+//  RecipeBytes-iOS
+//
+//  Created by Aimee Hong on 11/25/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeBytes_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
